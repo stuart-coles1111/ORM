d3180a868a62001dd2036c5a376e943aa5d1b97e
Stuart <40066813+stuart-coles1111@users.noreply.github.com>
2025-04-22 18:17:48 +0200

    Create diag.R

---
+++ dev/diag.R
@@ -0,0 +1,18 @@
+gum_fit <- ORM::fit("gum", 0, 4, ski_data,first_Run =FALSE, allranks=FALSE, maxrank=15, if_hessian=FALSE, init=0, method="BFGS", print=FALSE)
+exp_fit <- ORM::fit("exp", 0, 4, ski_data,first_Run =FALSE, allranks=FALSE, maxrank=15, if_hessian=FALSE, init=0, method="BFGS", print=FALSE)
+
+gum_preds <- c()
+
+for(i in 1:10){
+    preds <-ORM::pred(ski_data,i,gum_fit, plot=FALSE)
+    preds_df <- data.frame(race_number = i, rank1 = preds$Run1_pos, prob = preds$prob, rank2 = nrow(preds) - rank(preds$prob) +1, position = preds$position)
+    gum_preds <- rbind(gum_preds, preds_df)
+}
+
+exp_preds <- c()
+
+for(i in 1:10){
+    preds <-ORM::pred(ski_data,i,exp_fit, plot=FALSE)
+    preds_df <- data.frame(race_number = i, rank1 = preds$Run1_pos, prob = preds$prob, rank2 = nrow(preds) - rank(preds$prob) +1, position = preds$position)
+    exp_preds <- rbind(exp_preds, preds_df)
+}
